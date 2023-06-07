d26bdcc762d407d9002eb998e52e624f5648f3ef
VENHABRABO <97474096+VENHABRABO@users.noreply.github.com>
2023-06-07 11:16:14 -0400

    Add files via upload

---
+++ video/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<video width="320" height="240" controls="controls" autoplay="autoplay">
+<source src="../banner/video/tutorial.mp4" type="tutorial/mp4" />
+<!--Suportado em IE9, Chrome 6 e Safari 5 --> 
+O seu navegador não suporta a tag vídeo
+</video>
+
+</body>
+</html>
